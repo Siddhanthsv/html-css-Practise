8d1b00547f349f8f0b786c92708f9267d72a5c81
Siddhanthsv <siddhanthsv@gmail.com>
2026-02-26 19:00:25 +0530

    added

---
+++ trial.c
@@ -0,0 +1,37 @@
+
+<html>
+    
+<head>
+    <title>My First Web Page</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styletry.css">
+</head>
+
+<body>
+    <section class="container">
+
+  <section class="card">
+     <img src="sid.jpg" width="100">
+        <h2>Saathvika</h2>
+        <p>Web Learner</p>
+        <button>FOLLOW BUTTON</button>
+  </section>
+ <img src="sid.jpg" width="100">
+        <h2>Siddhanth Balaji</h2>
+        <p>Web Learner</p>
+        <button>FOLLOW BUTTON</button>
+  <section class="card">
+    <!-- copy same content -->
+  </section>
+
+  <section class="card">
+    <img src="sid.jpg" width="100">
+        <h2>Sridhar</h2>
+        <p>Web Learner</p>
+        <button>FOLLOW BUTTON</button>
+  </section>
+
+</section>
+</body>
+
+</html>
